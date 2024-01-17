628bca6d31158fddcb93eddda9dae41e9cd0707b
It21217654 <IT21217654@my.sliit.lk>
2024-01-18 01:12:20 +0530

    style binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
       welcome {{name}}
     </h2>
     <!-- <h2 [style.color]="'orange'">style binding</h2> -->
-    <h2 [style.color]="hasError ? 'red': 'green'">style binding</h2> 
+    <h2 [style.color]="hasError ? 'red': 'green'">style binding</h2>
+    <h2 [style.color]="highlightColor">style binding 2</h2> <!-- We can assign component class properties during binding -->
+    <h2 [ngStyle]="titleStyles">style binding 3</h2>
+
+
+
   `,
   styles:[]
 })
@@ -16,6 +21,11 @@ export class TestComponent implements OnInit{
   public name = "Rashini";
   public hasError=false;
   public isSpecial=true;
+  public highlightColor="yellow";
+  public titleStyles={ 
+    color:"blue",
+    fontStyle:"italic"
+  }
   
   constructor(){}
 
