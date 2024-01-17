409885d1f1a7e23ee17f86f8e5df7b62e63b0fe6
It21217654 <IT21217654@my.sliit.lk>
2024-01-18 01:02:43 +0530

    style binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,14 +6,15 @@ import { Component, OnInit } from '@angular/core';
     <h2>
       welcome {{name}}
     </h2>
-  
+    <!-- <h2 [style.color]="'orange'">style binding</h2> -->
+    <h2 [style.color]="hasError ? 'red': 'green'">style binding</h2> 
   `,
   styles:[]
 })
 export class TestComponent implements OnInit{
 
   public name = "Rashini";
-  public hasError=true;
+  public hasError=false;
   public isSpecial=true;
   
   constructor(){}
