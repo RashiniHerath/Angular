1d032a629c50790f6a14667e45f3d393442f125b
It21217654 <IT21217654@my.sliit.lk>
2024-01-18 21:33:27 +0530

    Initial commit

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,29 +6,21 @@ import { Component, OnInit } from '@angular/core';
     <h2>
       welcome {{ name }}
     </h2>
-    <button (click)="onClick($event)"> Greet </button>
-    <h2>{{ greeting }}</h2> <!-- Use interpolation and bind to the greeting property -->
-   <!-- <button (click)="greeting='Welcome Rashini'"> Greet </button>   Button triggers the direct assignment of the 'greeting' property without using an event handler -->
-
+    <input #myInput type="text">
+    <button (click)="logMessage(myInput.value)">Log</button>
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
   public name = "Rashini";
-  public greeting = "";
 
   constructor() {}
 
   ngOnInit() {}
 
-  // Event handler
-  onClick(event: any) {
-    console.log(event);
-    this.greeting = "Welcome Rashini"; // Set the property value
+  logMessage(value: any) {
+    console.log(value);
   }
 }
 
 
-
-//sometimes we need all information event itself. for that we can simply  send in a parameter in the event handler
-// ($event).it gives us all information about the DOM event that wass raised.
