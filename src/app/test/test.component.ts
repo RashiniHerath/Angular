f133db7e1b07557a7e0f059577c2739a500252da
It21217654 <IT21217654@my.sliit.lk>
2024-01-22 08:20:46 +0530

    Initial commit

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -4,22 +4,21 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-test',
   template: `
     <h2>
-      welcome {{ name }}
+     
     </h2>
-    <input [(ngModel)]="name" type="text">  
+   
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
-  public name: string = "Rashini"; 
+
 
   constructor() {}
 
   ngOnInit() {}
 
-  logMessage(value: any) {
-    console.log(value);
+ 
   }
-}
+
 
 
