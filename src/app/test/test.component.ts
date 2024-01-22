d7c765d338d096a1919e199cbba66a001dd6f67f
It21217654 <IT21217654@my.sliit.lk>
2024-01-22 09:18:46 +0530

    ngSwitch

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,23 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-   
+    <div [ngSwitch]="color">
+      <div *ngSwitchCase="'red'">You picked red</div>
+      <div *ngSwitchCase="'blue'">You picked blue</div>
+      <div *ngSwitchCase="'black'">You picked black</div>
+      <div *ngSwitchDefault>Pick again</div> <!-- default color -->
 
-
+    </div>
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
+  
+  public color = "green";
 
-  displayName=true;
   constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
 
-  }
-
- 
-  }
-
-
-
+}
