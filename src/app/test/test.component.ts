bd0f82ff4923787be1aa4ebf40e36630c0a080af
It21217654 <IT21217654@my.sliit.lk>
2024-01-22 08:49:29 +0530

    ngIf direcctive

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,19 +3,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-    <h2>
-     
+    <h2 *ngIf="displayName; else elseBlock"> <!-- //conditionally render the particular HTML event -->
+     Rashini
     </h2>
-   
+   <ng-template #elseBlock>
+    <h2>
+      Name is hidden
+</h2>
+</ng-template>
+
+<!-- <div *ngIf="displayName; then the Block; else elseBlock" -->
+
+<!-- <ng-template #thenBlock -->
+  <!-- <h2> Rashini </h2> -->
+<!-- <ng-template -->
+
+<!-- <ng-template #elseBlock -->
+  <!-- <h2> Hidden </h2> -->
+<!-- <ng-template -->
+
+
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
 
-
+  displayName=true;
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+
+  }
 
  
   }
