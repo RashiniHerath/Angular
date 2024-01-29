cfc83c7b161e3205b9d5aba26ee7c04e636b55c3
It21217654 <IT21217654@my.sliit.lk>
2024-01-29 23:10:36 +0530

    Route parameters

--- src/app/department-list/department-list.component.ts
+++ src/app/department-list/department-list.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-department-list',
-  templateUrl: './department-list.component.html',
-  styleUrl: './department-list.component.css'
+  template: `
+    <h3>
+      Department List
+    </h3>
+    <ul class="items">
+      <li *ngFor="let department of departments" [class.selected]="isSelected(department)" (click)="onSelect(department)">
+        <span class="badge">{{department.id}}</span> {{department.name}}
+      </li>
+    </ul>
+  `,
+  styles: []
 })
-export class DepartmentListComponent {
+export class DepartmentListComponent implements OnInit {
 
+  public selectedId!: number;
+  departments = [
+    {"id": 1, "name": "Angular"},
+    {"id": 2, "name": "Node"},
+    {"id": 3, "name": "MongoDB"},
+    {"id": 4, "name": "Ruby"},
+    {"id": 5, "name": "Bootstrap"}
+  ]
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      let idString = params.get('id'); // Get the id as a string
+      if (idString !== null) {
+        let id = parseInt(idString); // Parse the string to an integer
+        this.selectedId = id;
+      }
+    });
+  }
+
+  onSelect(department: { id: any; }) {
+    //this.router.navigate(['/departments', department.id]);
+    this.router.navigate([department.id], { relativeTo: this.route });
+  }
+
+  isSelected(department: { id: number; }) { 
+    return department.id === this.selectedId; 
+  }
 }
