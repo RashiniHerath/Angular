54f1e720293bce590ff03514b7ac9fe3439ee808
It21217654 <IT21217654@my.sliit.lk>
2024-01-29 23:27:05 +0530

    paramMap Observable

--- src/app/department-detail/department-detail.component.ts
+++ src/app/department-detail/department-detail.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-department-detail',
-  templateUrl: './department-detail.component.html',
-  styleUrl: './department-detail.component.css'
+  template: `
+    <h3> You selected department with id = {{departmentId}}</h3>
+    <a (click)="goPrevious()">Previous</a>
+    <a (click)="goNext()">Next</a>
+  `,
+  styles: []
 })
-export class DepartmentDetailComponent {
+export class DepartmentDetailComponent implements OnInit {
+  public departmentId: number | undefined;
 
+  constructor(private route: ActivatedRoute, private router: Router) { }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  goPrevious(): void {
+    if (this.departmentId !== undefined) {
+      let previousId = this.departmentId - 1;
+      this.router.navigate(['/departments', previousId]);
+    }
+  }
+
+  goNext(): void {
+    if (this.departmentId !== undefined) {
+      let nextId = this.departmentId + 1;
+      this.router.navigate(['/departments', nextId]);
+    }
+  }
 }
